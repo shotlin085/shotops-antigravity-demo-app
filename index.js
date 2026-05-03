c2eaed7d49b73ba757f0fb74578ef8f7a7ab7c68
Shotlin <shotlin085@gmail.com>
2026-05-03 20:55:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
       <body>
         <h1>ShotOps Safe Deploy QA</h1>
         <p>This is a disposable demo application.</p>
-        <div class="version">Version v2</div>
+        <div class="version">Version v1</div>
       </body>
     </html>
   `);
